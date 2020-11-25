173ab2b799eaa36c196bd99edad432fa1c910fca
Andrew Turner <turner.andrew0@gmail.com>
2020-11-25 15:45:12 -0600

    exercise complete

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -1,34 +1,60 @@
-const messages = []
-let id = messages[messages.length -1].id + 1
+let messages = []
+let id = 0
 
 
 module.exports = {
     
     readMessage(req, res){
         res.status(200).send(messages)
-    }
+    },
     
     createMessage(req, res){
         const {text, time} = req.body
         const newMessage = {
-            id
+            id,
             text,
             time,
         }
+        messages.push(newMessage)
+
+        id++
 
         res.status(200).send(messages)
-    }
+    },
 
     updateMessage(req, res){
+        const {message_id} = req.params
+        const {text, time} = req.body
+        const index = messages.findIndex(element => element.id === +message_id)
+
+        if(index === -1){
+            return res.status(404).send('<img src = "https://media.giphy.com/media/VIQfHC9jAZbt6ojTdo/giphy.gif"/>')
+        }
         
-        
+        const currentMessage = messages[index]
+
+        messages[index] = {
+                id: currentMessage.id,
+                text: text || currentMessage.text,
+                time: time || currentMessage.time,
+        }
+
         res.status(200).send(messages)
-    }
+    },
 
     deleteMessage(req, res){
-        
-        
-        res.status(200).send(messages)
-    }
+        const {message_id} = req.params
 
+        const index = messages.findIndex(element => element.id === +message_id)
+        
+        if(index !== -1){
+            messages.splice(index, 1)
+            res.status(200).send(messages)
+        } else {
+            return res.status(404).send('<img src = "https://media.giphy.com/media/VIQfHC9jAZbt6ojTdo/giphy.gif"/>')
+        }
+        
+
+
+    }
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
-const controller = require('messages_controller')
+const msgCtrl = require('./controllers/messages_controller')
 
 app.use(express.json())
+app.use(express.static('../public/build'))
 
 const port = 3001
+
+app.get('/api/messages', msgCtrl.readMessage)
+
+app.post('/api/messages/', msgCtrl.createMessage)
+
+app.put('/api/messages/:message_id', msgCtrl.updateMessage)
+
+app.delete('/api/messages/:message_id', msgCtrl.deleteMessage)
 
 app.listen(port, () => console.log(`${port} is open for business`))
